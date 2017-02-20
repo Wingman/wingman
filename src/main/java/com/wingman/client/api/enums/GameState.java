3299400a2708386a4868b8e7ed90fac846fc6943
UniquePassive <prozet@live.se>
2017-02-20 16:21:58 +0100

    Add quickhopping gamestate to GameState API

--- src/main/java/com/wingman/client/api/enums/GameState.java
+++ src/main/java/com/wingman/client/api/enums/GameState.java
@@ -7,6 +7,7 @@ public class GameState {
     public static final int LOADING = 25;
     public static final int PLAYING = 30;
     public static final int DISCONNECTED = 40;
+    public static final int QUICKHOPPING = 45;
 
     private GameState() {
         // This class should not be instantiated
